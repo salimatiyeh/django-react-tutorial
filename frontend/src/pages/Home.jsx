5e50f9f81b980931241c538920cb9ab86b80c585
Salim Atiyeh <salimatiyeh@gmail.com>
2025-02-08 18:46:50 -0600

    created the createNote function on Home.jsx page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -19,8 +19,56 @@ function Home() {
       .catch((err) => alert(err));
   };
 
+  const deleteNote = (id) => {
+    api.delete(`/api/notes/${id}/`).then((res) => {
+      if (res.status === 204) alert("Note deleted!")
+        else alert("Failed to delete note.")
+      getNotes();
+    }).catch((error) => alert(error))
+  }
 
-  return <div>Home Page</div>
-}
+  const createNote = (e) => {
+    e.preventDefault()
+    api.post("/api/notes/", {content, title}).then((res) => {
+      if (res.status === 201) alert("Note created!")
+        else alert("Failed to make note.")
+      getNotes();
+    })
+    .catch((err) => alert(err));
+  }
+
+
+  return <div>
+          <div>
+            <h2>Notes</h2>
+          </div>
+          <div>
+            <h2>Create a Note</h2>
+            <form onSubmit={createNote}>
+              <label htmlFor="title">Title:</label>
+            <br/>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              required
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+            />
+            <label htmlFor="content">Content:</label>
+            <br />
+            <textarea
+              id="content"
+              name="content"
+              required
+              onChange={(e) => setContent(e.target.value)}
+              value={content}
+            ></textarea>
+            <br />
+            <input type="submit" value="submit"></input>
+            </form>
+          </div>
+        </div>
+};
 
 export default Home;
